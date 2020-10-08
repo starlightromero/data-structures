27129872c7a5e2602f5f48d2b735b3f887bb04b9
Starlight Romero <starlightromero@gmail.com>
2020-10-08 10:18:50 -0700

    moved linked-stack

---
+++ lists/linked-stack.js
@@ -0,0 +1,34 @@
+import { LinkedList } from './linked-list.js'
+
+class LinkedStack {
+  constructor () {
+    this.list = new LinkedList()
+  }
+
+  push (value) {
+    this.list.prepend(value)
+  }
+
+  pop () {
+    return this.list.deleteHead()
+  }
+
+  isEmpty () {
+    return !this.list.head
+  }
+
+  toArray () {
+    return this.list.toArray()
+  }
+}
+
+const linkedStack = new LinkedStack()
+linkedStack.push('Cook dinner!')
+linkedStack.push('Wash the dishes!')
+linkedStack.push('Buy ingredients!')
+console.log(linkedStack.toArray())
+console.log(linkedStack.pop())
+console.log(linkedStack.toArray())
+console.log(linkedStack.pop())
+console.log(linkedStack.pop())
+console.log(linkedStack.toArray())
